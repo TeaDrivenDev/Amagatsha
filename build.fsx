3cc96b2aa211804895dbd4f819ad958aac97ecee
TeaDrivenDev <TeaDrivenDev@gmail.com>
2017-06-24 03:38:40 +0200

    Bump version to 1.0.0

--- build.fsx
+++ build.fsx
@@ -2,11 +2,18 @@ System.IO.Directory.SetCurrentDirectory __SOURCE_DIRECTORY__
 
 #r @"packages/build/FAKE/tools/FakeLib.dll"
 
+open System
 open System.IO
 
 open Fake
+open Fake.Git
 
 let solutionFile  = "Amagatsha.sln"
+
+let gitOwner = "TeaDrivenDev"
+let gitHome = "https://github.com/" + gitOwner
+let gitName = "Amagatsha"
+let gitRaw = environVarOrDefault "gitRaw" ("https://raw.github.com/" + gitOwner)
 
 let outputDirectory = "bin"
 
@@ -34,8 +41,55 @@ Target "Build" (fun _ ->
     |> MSBuildRelease "" "Rebuild"
     |> ignore)
 
+Target "Pack" (fun _ ->
+    let filesToPack =
+        {
+            BaseDirectory = outputDirectory
+            Includes = [ "*.exe"; "*.dll"; "*.config" ]
+            Excludes = []
+        }
+
+    ZipOfIncludes (outputDirectory + "/Amagatsha.zip") ["", filesToPack ]
+)
+
+#load "paket-files/build/fsharp/FAKE/modules/Octokit/Octokit.fsx"
+open Octokit
+
+Target "ReleaseGitHub" (fun _ ->
+    let user =
+        match getBuildParam "github-user" with
+        | s when not (String.IsNullOrWhiteSpace s) -> s
+        | _ -> getUserInput "Username: "
+    let pw =
+        match getBuildParam "github-pw" with
+        | s when not (String.IsNullOrWhiteSpace s) -> s
+        | _ -> getUserPassword "Password: "
+    let remote =
+        Git.CommandHelper.getGitResult "" "remote -v"
+        |> Seq.filter (fun (s: string) -> s.EndsWith("(push)"))
+        |> Seq.tryFind (fun (s: string) -> s.Contains(gitOwner + "/" + gitName))
+        |> function None -> gitHome + "/" + gitName | Some (s: string) -> s.Split().[0]
+
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
+    Branches.pushBranch "" remote (Information.getBranchName "")
+
+    Branches.tag "" release.NugetVersion
+    Branches.pushTag "" remote release.NugetVersion
+    
+    // release on github
+    createClient user pw
+    |> createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes 
+    |> uploadFile "./bin/Amagatsha.zip"
+    |> releaseDraft
+    |> Async.RunSynchronously
+)
+
+
 "AssemblyInfo"
 ==> "Clean"
 ==> "Build"
+==> "Pack"
+==> "ReleaseGitHub"
 
 RunTargetOrDefault "Build"
